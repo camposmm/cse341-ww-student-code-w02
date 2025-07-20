53d835345e9db4b45742c1c46bd6b9dd87134fa2
camposmm <mcamposdossantos@byupathway.edu>
2025-07-19 21:44:00 -0300

    week03

--- models/temple.js
+++ models/temple.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const templeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  location: String,
-  dedication: String,
-  services: [String],
+  location: { type: String, required: true },
+  dedicated: { type: Date },
+  area: { type: String },
+  services: { type: [String] }
 });
 
 module.exports = mongoose.model('Temple', templeSchema);
